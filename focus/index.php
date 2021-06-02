af5261307d36b36196feb103776209984316754d
lakmalsamarakkody <43323601+lakmalsamarakkody@users.noreply.github.com>
2021-06-02 17:54:33 +0530

    Add files via upload

---
+++ focus/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/styles.css"/>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+	<div id="mainSection" style="background:url(imgs/login_back.jpg)">
+    	<header><span id="hea">Garbage Management System</span></header>
+    	<table width="600px" height="300px" id="loginTable"> 
+        <form action="" method="post">
+        	<tr>
+            	<td colspan="2">
+                	<span>General Log-in</span>
+            	</td>
+            </tr>
+            	<td>Username</td><td><input type="text" name="uname" required/></td>
+                
+            <tr>
+            	<td>Password</td><td><input type="password" name="pass" required/></td>
+            </tr>
+            <tr>
+            		<td colspan="2" align="right">
+                    	<input type="submit" name="login" value="Log-in" id="btn"/>
+                    </td>
+            </tr>
+            </form>
+        </table>
+    
+    </div>
+    <div id="mainSection" style="background-color:#999">
+    </div>	
+
+</body>
+</html>
